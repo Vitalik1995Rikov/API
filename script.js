7f10d1444383a5e04433ee7b341a00fc490810ab
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-13 14:08:03 +0300

    подключил апи погоды

---
+++ script.js
@@ -0,0 +1,6 @@
+let url = 'http://api.weatherstack.com/current?access_key=17a677e3fd280a4a116ada9093dfdea0&query=London';
+
+fetch(url)
+    .then(response => response.json())
+    .then(result => console.log(result))
+
