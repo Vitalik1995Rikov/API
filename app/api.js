93d62e84f32fb99d7538f44ac5652fd946e93118
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-14 16:21:53 +0300

    надо настроить промисы

--- app/api.js
+++ app/api.js
@@ -1,6 +1,9 @@
-let url = 'http://api.weatherstack.com/current?access_key=17a677e3fd280a4a116ada9093dfdea0&query=Gorkey';
+let url = 'http://api.weatherstack.com/current?access_key=17a677e3fd280a4a116ada9093dfdea0&query=Nizhny Novgorod';
+// let url = 'http://dataservice.accuweather.com/locations/v1/search'
 
 fetch(url)
-    .then(response => response.json())
-    .then(result => console.log(result))
+    .then(response =>response.json())
+    .then(result => console.log(result.location.name))
+
+
 
