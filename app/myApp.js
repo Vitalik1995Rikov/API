8ef8aa898a379416a830e383d020fd666eafc8bb
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-15 17:51:53 +0300

    добавил scope

--- app/myApp.js
+++ app/myApp.js
@@ -1,5 +1,12 @@
 let myApp = angular.module('myApp', []);
 myApp.controller('myController', function($scope) {
-    $scope.name = 'alex';
-});
+    $scope.object = {
+            type: "City",
+            query: "Nizhny Novgorod",
+            country: "Russian Federation",
+            language: "en",
+            unit: "m"
+        }
+    }
+);
 
