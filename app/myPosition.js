210788796325b9582d45f6fd98a91fc5cde56132
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-16 15:56:10 +0300

    как же вытащить scope

--- app/myPosition.js
+++ app/myPosition.js
@@ -1,19 +1,45 @@
 let myApp = angular.module('myApp', []);
 myApp.controller('myPosition', function($scope) {
-    
+
+
     navigator.geolocation.getCurrentPosition(position => {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        console.log(latitude, longitude);
-    
+        const { latitude, longitude } = position.coords;
+
     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
-    xhr.send();
-    $scope.data = JSON.parse(xhr.response);
-    
-    console.log($scope.data);
-    return $scope.data;
+    fetch(url)
+        .then(response =>response.json())
+        // .then(result => console.log(result))
+        .then(result => {$scope.data = result})
+        .then(() => console.log($scope.data))
     });
 });
+
+
+
+
+
+
+
+
+// let myApp = angular.module('myApp', []);
+// myApp.controller('myPosition', function($scope) {
+
+//     navigator.geolocation.getCurrentPosition(position => {
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//         console.log(latitude, longitude);
+//         return latitude;
+//     });
+//     console.log(latitude)
+        
+//     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
+//     let xhr = new XMLHttpRequest();
+//     xhr.open("GET", url, false);
+//     xhr.send();
+//     $scope.data = JSON.parse(xhr.response);
+    
+//     console.log($scope.data);
+//     return $scope.data;
+// });
+
 
