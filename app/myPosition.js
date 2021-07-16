bb8c0d5fd3d47e8eef49b456490d1bfc40119f29
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-16 13:01:22 +0300

    разобраться с промисами

---
+++ app/myPosition.js
@@ -0,0 +1,40 @@
+// let myPosition = angular.module('myPosition', []);
+// myPosition.controller('myPosition', function($scope) {
+
+
+//     navigator.geolocation.getCurrentPosition(position => {
+//         const { latitude, longitude } = position.coords;
+
+//     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
+//     fetch(url)
+//         .then(response =>response.json())
+//         // .then(result => console.log(result))
+//         .then(result => {$scope.data = result})
+//         .then(() => console.log($scope.data))
+//     });
+// });
+
+
+
+let myApp = angular.module('myApp', []);
+myApp.controller('myPosition', function($scope) {
+
+    // let latitude = 0;
+    // let longitude = 0;
+
+    navigator.geolocation.getCurrentPosition(position => {
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        console.log(latitude, longitude);
+        
+   
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.send();
+    $scope.data = JSON.parse(xhr.response);
+    
+    console.log($scope.data);
+});
+});
--- app/position.js
+++ app/position.js
@@ -3,8 +3,8 @@ navigator.geolocation.getCurrentPosition(position => {
     console.log(latitude, longitude);
 
      
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&cnt=10&appid=a1c23503e3844c136140a0cb126e42d2`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
     fetch(url)
         .then(response =>response.json())
-        .then(result => console.log(result.name))
+        .then(result => console.log(result))  
   });
