610ad3539f4a085e74a69f3b0a4565e78971f8ed
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-20 11:44:07 +0300

    понял как картинки стягивать с апи

--- app/myApp.module.js
+++ app/myApp.module.js
@@ -7,7 +7,7 @@ myApp.controller('myPosition', function($scope) {
     navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords;
 
-    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
+    let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
     fetch(url)
         .then(response => response.json())
         .then(result => {$scope.data = result})
@@ -15,22 +15,5 @@ myApp.controller('myPosition', function($scope) {
     });
     console.log($scope);
 });
-
-
-
-// myApp.directive('weatherCard', function() {
-//     return {
-//         templateUrl: "templates/templateWeatherCard.html",
-//     }
-// });
-
-
-
-
-// myApp.directive('townCard', function() {
-//     return {
-//         templateUrl: "templates/templateTownCard.html",
-//     }
-// });
 
 
