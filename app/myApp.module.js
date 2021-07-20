4dfea1ea8758bb51ba84b03e4bf7df85cbc98c42
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-20 12:36:37 +0300

    пробую получать на несколько дней

--- app/myApp.module.js
+++ app/myApp.module.js
@@ -2,8 +2,23 @@ let myApp = angular.module('myApp', ['ngMaterial', 'app.weather', 'app.towns']);
 
 
 
-myApp.controller('myPosition', function($scope) {
+// myApp.controller('myPosition', function($scope) {
         
+//     navigator.geolocation.getCurrentPosition(position => {
+//         const { latitude, longitude } = position.coords;
+
+//     let url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a1c23503e3844c136140a0cb126e42d2`;
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(result => {$scope.data = result})
+//         .then(() => console.log($scope.data.name))
+//     });
+//     console.log($scope);
+// });
+
+
+
+myApp.controller('fiveDays', function($scope) {
     navigator.geolocation.getCurrentPosition(position => {
         const { latitude, longitude } = position.coords;
 
@@ -11,9 +26,8 @@ myApp.controller('myPosition', function($scope) {
     fetch(url)
         .then(response => response.json())
         .then(result => {$scope.data = result})
-        .then(() => console.log($scope.data.name))
+        .then(() => console.log($scope.data.daily[0].weather[0].icon))
     });
-    console.log($scope);
+    console.log($scope)
 });
-
 
