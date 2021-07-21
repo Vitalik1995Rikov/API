a02d341a072b07c554fad5eb187dff46da93e3b7
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-21 12:49:53 +0300

    починил карточки погоды

--- app/directives/app.weather.js
+++ app/directives/app.weather.js
@@ -2,6 +2,13 @@ angular.module('app.weather', []);
 
 angular.module('app.weather').directive('weatherCard', function() {
     return {
-        templateUrl: "templates/templateWeatherCard.html"
+        scope: true,
+        templateUrl: "templates/templateWeatherCard.html",
+        link: function ($scope) {
+                let url = `http://api.openweathermap.org/data/2.5/onecall?lat=56.2&lon=44&appid=a1c23503e3844c136140a0cb126e42d2`;
+                    fetch(url)
+                        .then(response => response.json())
+                        .then(result => {$scope.data = result})
+        }
     }
 });
