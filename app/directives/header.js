c5bce38c947066184e19c5e026c649c023acbd41
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-21 18:14:31 +0300

    незначительные изменения, можно убрать

--- app/directives/header.js
+++ app/directives/header.js
@@ -2,6 +2,15 @@ angular.module('appHeader', []);
 
 angular.module('appHeader').directive('appHeader', function() {
     return {
+        scope: true,
         templateUrl: "templates/templateAppHeader.html",
+        link: function (scope, valueInput) {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&appid=a1c23503e3844c136140a0cb126e42d2`;
+                fetch(url)
+                    .then(response => response.json())
+                    .then(result => {scope.data = result})
+                    .then(() => console.log(result))
+        }
+
     }
 });
