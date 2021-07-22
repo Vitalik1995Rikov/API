97016e599a54f830a93b56b1f06b7ce0d5b04c21
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-22 13:26:11 +0300

    по кнопке выводит погоду

--- app/directives/header.js
+++ app/directives/header.js
@@ -2,15 +2,22 @@ angular.module('appHeader', []);
 
 angular.module('appHeader').directive('appHeader', function() {
     return {
-        scope: true,
-        templateUrl: "templates/templateAppHeader.html",
-        link: function (scope, valueInput) {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${valueInput}&appid=a1c23503e3844c136140a0cb126e42d2`;
+        templateUrl: "templates/templateAppHeader.html"
+    }
+});
+
+
+
+angular.module('appHeader').controller('appHeaderCtrl', function($scope) {
+    
+    $scope.valueInput = 'London';
+    url = `http://api.openweathermap.org/data/2.5/weather?q=${$scope.valueInput}&appid=a1c23503e3844c136140a0cb126e42d2`;
+    
+
+    $scope.oneTown = function($scope) {
                 fetch(url)
                     .then(response => response.json())
-                    .then(result => {scope.data = result})
-                    .then(() => console.log(result))
-        }
-
+                    .then(result => {$scope = result})
+                    .then(() => console.log($scope))
     }
 });
