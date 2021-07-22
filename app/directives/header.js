e069034b40b2454a5cffb89d9d6945d7a10cbbef
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-22 14:21:01 +0300

    с инпута выводит в консоль выбранный город

--- app/directives/header.js
+++ app/directives/header.js
@@ -10,14 +10,14 @@ angular.module('appHeader').directive('appHeader', function() {
 
 angular.module('appHeader').controller('appHeaderCtrl', function($scope) {
     
-    $scope.valueInput = 'London';
-    url = `http://api.openweathermap.org/data/2.5/weather?q=${$scope.valueInput}&appid=a1c23503e3844c136140a0cb126e42d2`;
+    $scope.valueInput = '';
     
-
-    $scope.oneTown = function($scope) {
+    $scope.oneTown = function() {
+        url = `http://api.openweathermap.org/data/2.5/weather?q=${$scope.valueInput}&appid=a1c23503e3844c136140a0cb126e42d2`;
                 fetch(url)
                     .then(response => response.json())
                     .then(result => {$scope = result})
                     .then(() => console.log($scope))
+    $scope.valueInput = '';
     }
 });
